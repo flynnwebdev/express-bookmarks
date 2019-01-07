e3e7e781a17171ad715a64326460c52749047f09
Garret Blankenship <garret@leafcutter.com.au>
2019-01-07 13:48:24 +1100

    code for class

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
+
+app.use(cors({
+    origin: process.env.FRONT_END_DOMAIN
+}));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
---
+++ controllers/bookmark_controller.js
@@ -0,0 +1,13 @@
+async function create(req, res, next) {
+    req.user.bookmarks.push(req.body);
+    try {
+        await req.user.save();
+        res.json(req.user.bookmarks);
+    } catch( err ) {
+        next(err);
+    }
+}
+
+module.exports = {
+    create
+}
